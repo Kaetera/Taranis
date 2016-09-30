af217de6b7ff646c5a26dde5386e64787a03e1ee
Quentin Chouiller <quentin.chouiller@grenoble-inp.org>
2016-09-30 22:01:03 +0200

    Perfomed some refactoring

--- app/src/main/java/kaetera/taranis/Main.java
+++ app/src/main/java/kaetera/taranis/Main.java
@@ -17,20 +17,21 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 
-import java.util.List;
 
 
 public class Main extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
-    //GUI items
-    private VerticalSeekBar seekBarThreshold = null;
-    private ImageButton imgBtnKeepDetected = null;
-    private TextView txtViewTest = null;
 
+    //GUI items
+    private VerticalSeekBar mSeekBarThreshold = null;
+    private ImageButton mImgBtnKeepDetected = null;
     private CameraBridgeViewBase mOpenCvCameraView = null;
-    private int counter = 0;
-    private static final String TAG = "OCVSample::Activity";
+    private TextView mTxtViewTest = null;
+
+    //Other globals
+    private int mCounter = 0;
+    private static final String TAG = "Taranis::Main";
+
 
 
     @Override
@@ -40,63 +41,52 @@ public class Main extends AppCompatActivity implements CameraBridgeViewBase.CvCa
         setContentView(R.layout.activity_main);
 
         //Get java objects
-        seekBarThreshold = (VerticalSeekBar) this.findViewById(R.id.seekBarThreshold);
-        imgBtnKeepDetected = (ImageButton) this.findViewById(R.id.imgBtnKeepDetected);
-        txtViewTest = (TextView) this.findViewById(R.id.txtViewTest);
+        mSeekBarThreshold = (VerticalSeekBar) this.findViewById(R.id.seekBarThreshold);
+        mImgBtnKeepDetected = (ImageButton) this.findViewById(R.id.imgBtnKeepDetected);
+        mTxtViewTest = (TextView) this.findViewById(R.id.txtViewTest);
         mOpenCvCameraView = (CameraBridgeViewBase) this.findViewById(R.id.cameraViewCV);
 
 
         //Set listeners
-        seekBarThreshold.setOnSeekBarChangeListener(this.onSeekBarThresholdChange);
-        imgBtnKeepDetected.setOnClickListener(this.onKeepDetectedClick);
+        mSeekBarThreshold.setOnSeekBarChangeListener(this.onSeekBarThresholdChange);
+        mImgBtnKeepDetected.setOnClickListener(this.onKeepDetectedClick);
         mOpenCvCameraView.setCvCameraViewListener(this);
-
-        //temp
-        if(mOpenCvCameraView != null) {
-            //this.mOpenCvCameraView.setMinimumWidth(1000);
-        }
-
     }
 
 
+
     @Override
-    public void onPause()
-    {
+    public void onPause() {
         super.onPause();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
+        if (mOpenCvCameraView != null) mOpenCvCameraView.disableView();
     }
 
 
+
     @Override
-    public void onResume()
-    {
+    public void onResume() {
         super.onResume();
         if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, this.mLoaderCallback);
         } else {
             Log.d(TAG, "OpenCV library found inside package. Using it!");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+            this.mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
     }
+
 
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
+        if (mOpenCvCameraView != null) mOpenCvCameraView.disableView();
     }
 
 
 
-
-    //Other stuff
-
-    /* Download and install OpenCV if not already present on the device. Avoid including OpenCV
-     * lib in the release.
-     */
+    //Download and install OpenCV if not already present on the device. Avoid including OpenCV
+    //lib in the release.
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -116,22 +106,19 @@ public class Main extends AppCompatActivity implements CameraBridgeViewBase.CvCa
 
 
 
-
+    //Stores the image where a lightning has been detected when the user presses the
+    //validation button in manual mode.
     private View.OnClickListener onKeepDetectedClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            counter++;
-            txtViewTest.setText("mCounter = " + Integer.toString(counter));
+            mCounter++;
+            mTxtViewTest.setText("mCounter = " + Integer.toString(mCounter));
         }
     };
 
 
 
-
-
-
-
-
+    //Adjusts the sensitivity threshold of the lightning detection algorithm.
     //The custom implementation of VerticalSeekBar need OnSeekBarChangeListener before using it in
     //the app, otherwise interacting with the VerticalSeekBar will produce NullPointerException.
     private SeekBar.OnSeekBarChangeListener onSeekBarThresholdChange = new SeekBar.OnSeekBarChangeListener() {
@@ -149,20 +136,6 @@ public class Main extends AppCompatActivity implements CameraBridgeViewBase.CvCa
 
 
 
-    //OpenCV stuff
-
-    //Implement CvCameraViewListener2
-    public void onCameraViewStarted(int width, int height) {
-    }
-
-
-    //Implement CvCameraViewListener2
-    public void onCameraViewStopped() {
-    }
-
-
-
-
     //Implements CvCameraViewListener2
     //Callback function called on each frame received. Returns as a matrix the image that will
     //be displayed in the JavaCameraView surface widget.
@@ -176,7 +149,7 @@ public class Main extends AppCompatActivity implements CameraBridgeViewBase.CvCa
         runOnUiThread(new Runnable() {
             public void run() {
                 //HERE I WANT TO UPDATE MY TEXT VIEW
-                txtViewTest.setText( s );
+                mTxtViewTest.setText( s );
             }
         });
 
@@ -185,5 +158,14 @@ public class Main extends AppCompatActivity implements CameraBridgeViewBase.CvCa
 
 
 
+    //Implements CvCameraViewListener2
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+
+
+    //Implements CvCameraViewListener2
+    public void onCameraViewStopped() {
+    }
 
 }//End Main.java
